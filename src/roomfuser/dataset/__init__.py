0989a0de62068a9de7af52687c425af52d38860f
Eric Grinstein <eric.grinstein@outlook.com>
2023-08-16 11:49:00 +0100

    N workers

--- src/roomfuser/dataset/__init__.py
+++ src/roomfuser/dataset/__init__.py
@@ -17,8 +17,6 @@ import numpy as np
 import os
 import random
 import torch
-import torch.nn.functional as F
-import torchaudio
 
 from torch.utils.data.distributed import DistributedSampler
 from torch.utils.data import DataLoader
@@ -27,93 +25,67 @@ from .random_sinusoid_dataset import RandomSinusoidDataset
 
 
 class Collator:
-  def __init__(self, params):
-    self.params = params
+    def __init__(self, params):
+        self.params = params
 
-  def collate(self, minibatch):
-    samples_per_frame = self.params.hop_samples
-    for record in minibatch:
-      if self.params.unconditional:
-          # Filter out records that aren't long enough.
-          if len(record['audio']) < self.params.audio_len:
-            del record['conditioner']
-            del record['audio']
-            continue
+    def collate(self, minibatch):
+        samples_per_frame = self.params.hop_samples
+        for record in minibatch:
+            if self.params.unconditional:
+                # Filter out records that aren't long enough.
+                if len(record["audio"]) < self.params.audio_len:
+                    del record["conditioner"]
+                    del record["audio"]
+                    continue
 
-          # Crop bigger records to audio_len.
-          start = random.randint(0, record['audio'].shape[-1] - self.params.audio_len)
-          end = start + self.params.audio_len
-          record['audio'] = record['audio'][start:end]
-          record['audio'] = np.pad(record['audio'], (0, (end - start) - len(record['audio'])), mode='constant')
+                # Crop bigger records to audio_len.
+                start = random.randint(
+                    0, record["audio"].shape[-1] - self.params.audio_len
+                )
+                end = start + self.params.audio_len
+                record["audio"] = record["audio"][start:end]
+                record["audio"] = np.pad(
+                    record["audio"],
+                    (0, (end - start) - len(record["audio"])),
+                    mode="constant",
+                )
 
-    audio = np.stack([record['audio'] for record in minibatch if 'audio' in record])
-    if self.params.unconditional:
+        audio = np.stack([record["audio"] for record in minibatch if "audio" in record])
+        if self.params.unconditional:
+            return {
+                "audio": torch.from_numpy(audio),
+                "conditioner": None,
+            }
+        spectrogram = np.stack(
+            [record["conditioner"] for record in minibatch if "conditioner" in record]
+        )
         return {
-            'audio': torch.from_numpy(audio),
-            'conditioner': None,
+            "audio": torch.from_numpy(audio),
+            "conditioner": torch.from_numpy(spectrogram),
         }
-    spectrogram = np.stack([record['conditioner'] for record in minibatch if 'conditioner' in record])
-    return {
-        'audio': torch.from_numpy(audio),
-        'conditioner': torch.from_numpy(spectrogram),
-    }
-
-  # for gtzan
-  def collate_gtzan(self, minibatch):
-    ldata = []
-    mean_audio_len = self.params.audio_len # change to fit in gpu memory
-    # audio total generated time = audio_len * sample_rate
-    # GTZAN statistics
-    # max len audio 675808; min len audio sample 660000; mean len audio sample 662117
-    # max audio sample 1; min audio sample -1; mean audio sample -0.0010 (normalized)
-    # sample rate of all is 22050
-    for data in minibatch:
-      if data[0].shape[-1] < mean_audio_len:  # pad
-        data_audio = F.pad(data[0], (0, mean_audio_len - data[0].shape[-1]), mode='constant', value=0)
-      elif data[0].shape[-1] > mean_audio_len:  # crop
-        start = random.randint(0, data[0].shape[-1] - mean_audio_len)
-        end = start + mean_audio_len
-        data_audio = data[0][:, start:end]
-      else:
-        data_audio = data[0]
-      ldata.append(data_audio)
-    audio = torch.cat(ldata, dim=0)
-    return {
-          'audio': audio,
-          'conditioner': None,
-    }
 
 
 def from_path(data_dirs, params, is_distributed=False):
-  if params.dataset_name == 'sinusoid':
-    dataset = RandomSinusoidDataset(n_sample=params.audio_len,
-                                    n_samples_per_epoch=params.n_samples_per_epoch)
-  else:
-    from .random_rir_dataset import RandomRirDataset
-    dataset = RandomRirDataset(
-      n_rir=params.audio_len,
-      n_samples_per_epoch=params.n_samples_per_epoch
+    if params.dataset_name == "sinusoid":
+        dataset = RandomSinusoidDataset(
+            n_sample=params.audio_len, n_samples_per_epoch=params.n_samples_per_epoch
+        )
+    elif params.dataset_name == "rir":
+        from .random_rir_dataset import RandomRirDataset
+
+        dataset = RandomRirDataset(
+            n_rir=params.audio_len, n_samples_per_epoch=params.n_samples_per_epoch
+        )
+    else:
+        raise NotImplementedError(f"Unknown dataset: {params.dataset_name}")
+
+    return DataLoader(
+        dataset,
+        batch_size=params.batch_size,
+        collate_fn=Collator(params).collate,
+        shuffle=not is_distributed,
+        num_workers=os.cpu_count() if params.n_workers is None else params.num_workers,
+        sampler=DistributedSampler(dataset) if is_distributed else None,
+        pin_memory=True,
+        drop_last=True,
     )
-
-  return DataLoader(
-      dataset,
-      batch_size=params.batch_size,
-      collate_fn=Collator(params).collate,
-      shuffle=not is_distributed,
-      num_workers=os.cpu_count(),
-      sampler=DistributedSampler(dataset) if is_distributed else None,
-      pin_memory=True,
-      drop_last=True)
-
-
-def from_gtzan(params, is_distributed=False):
-  dataset = torchaudio.datasets.GTZAN('./data', download=True)
-  return DataLoader(
-      dataset,
-      batch_size=params.batch_size,
-      collate_fn=Collator(params).collate_gtzan,
-      shuffle=not is_distributed,
-      num_workers=os.cpu_count(),
-      sampler=DistributedSampler(dataset) if is_distributed else None,
-      pin_memory=True,
-      drop_last=True)
